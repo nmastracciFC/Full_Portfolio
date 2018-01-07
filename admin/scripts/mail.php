f036a4c3739785cea78a6dc49648dcc0721a7d92
nmastracci <mastracci.natalie@gmail.com>
2018-01-06 22:50:35 -0500

    checking over work

--- admin/scripts/mail.php
+++ admin/scripts/mail.php
@@ -13,7 +13,7 @@ function redirect_to($location){
 
 function submitMessage($name, $email, $message, $direct) {
 //VERY IMPORTANT that these variables are in the same order becasue this is the order they will be enteredintop the database
-	//MAMP not locally set p fpor email so you have to test live
+	//MAMP not locally set up for email so you have to test live
 	$to = "admin@nataliemastracci.com"; //who the email is going to ALWAYS hosting
 	$subj = "Message from Portfolio Form"; //this prevents it from looking like spam
 	$extra = "Reply-To: ".$email; //gives you the email to respond to. Click it in the body of the email to be able to respond to the person and not to yourself. Do not click reply it will email yourself.
